f360e49d3f4a4d160a95e604278be1ba8ded060d
demlt <demlt@bswiesaude.onmicrosoft.com>
2026-07-14 10:52:49 +0200

    unnotwendige vues gelöscht

--- src/views/admin/hauptseiten/hundewahl.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <div>Platzhalter für diese Seite</div>
-</template>
--- src/views/admin/test.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="p-6 text-center">
-    <h1 class="text-4xl font-bold text-green-600 animate-bounce">Hallo! 👋</h1>
-    <p class="text-gray-500 mt-2">
-      Wenn du das siehst, funktioniert dein Vue-Router einwandfrei. Admin Seite!
-    </p>
-  </div>
-</template>
