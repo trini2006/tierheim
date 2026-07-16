b6a0d066b0243f78d34b4943b3ceb9a9631b8618
grossc2 <grossc2@bswiesaude.onmicrosoft.com>
2026-07-16 15:29:43 +0200

    idk start halt

---
+++ backend/tierheim/src/main/java/projekt_tierheim/tierheim/WebConfig.java
@@ -0,0 +1,16 @@
+package projekt_tierheim.tierheim;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMapping(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+}
