b524447328ec922865d4508f73d1d9a099cab81a
grossc2 <grossc2@bswiesaude.onmicrosoft.com>
2026-07-20 16:08:41 +0200

    Label Getter, Setter, Konstruktor, etc.

--- backend/tierheim/src/main/java/projekt_tierheim/tierheim/db/Label/Label.java
+++ backend/tierheim/src/main/java/projekt_tierheim/tierheim/db/Label/Label.java
@@ -5,6 +5,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+import java.util.Objects;
+
 @Entity
 public class Label {
 
@@ -16,5 +18,47 @@ public class Label {
     // 0 = normal, 1 = wichtig
     private boolean hinweis;
 
+    public Label(){}
 
+    public Label(int id, String bezeichnung, boolean hinweis) {
+        this.id = id;
+        this.bezeichnung = bezeichnung;
+        this.hinweis = hinweis;
+    }
+
+    public String getBezeichnung() {
+        return bezeichnung;
+    }
+
+    public void setBezeichnung(String bezeichnung) {
+        this.bezeichnung = bezeichnung;
+    }
+
+    public boolean isHinweis() {
+        return hinweis;
+    }
+
+    public void setHinweis(boolean hinweis) {
+        this.hinweis = hinweis;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Label label = (Label) o;
+        return getId() == label.getId() && isHinweis() == label.isHinweis() && Objects.equals(getBezeichnung(), label.getBezeichnung());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getBezeichnung(), isHinweis());
+    }
 }
