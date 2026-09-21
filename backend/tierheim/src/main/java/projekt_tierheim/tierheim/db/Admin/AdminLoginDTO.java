1b92ccde781313a98df51f2f71f83d010cc26c92
grossc2 <grossc2@bswiesaude.onmicrosoft.com>
2026-09-21 09:09:31 +0200

    Admin DTOs fertig + Login Logik eingebaut für Admin

---
+++ backend/tierheim/src/main/java/projekt_tierheim/tierheim/db/Admin/AdminLoginDTO.java
@@ -0,0 +1,10 @@
+package projekt_tierheim.tierheim.db.Admin;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+public record AdminLoginDTO (
+        @NotNull @Positive int personalnummer,
+        @NotBlank String passwort)
+{ }
